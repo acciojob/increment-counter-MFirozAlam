d574dafb3de354409d2e225788d5b5d09eb67166
MFirozAlam <146247719+MFirozAlam@users.noreply.github.com>
2026-05-21 20:20:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-//your JS code here. If required.
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Increment Counter</title>
+</head>
+<body>
+
+    <p id="counter">0</p>
+
+    <button id="incrementBtn">Increment</button>
+
+    <script>
+        let count = 0;
+
+        document.getElementById("incrementBtn").addEventListener("click", function () {
+
+            alert(count);
+
+            count++;
+
+            document.getElementById("counter").innerText = count;
+        });
+    </script>
+
+</body>
+</html>
